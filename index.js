daa787e7fa0b456bc9ccebb576c7822e1cedb914
sankar kalla <gowrisankarkalla4@gmail.com>
2025-09-11 22:19:40 +0530

    feature: add CI pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log('Hello world');
+console.log("i'm learning CI/CD pipelines through github actions");
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log('started testing')
+setTimeout(() => {
+    console('wait, testing in progress');
+}, 3000);
+console.log('testing completed')
