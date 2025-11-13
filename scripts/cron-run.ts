9e8efe97a4ec1a33cbb76e5a5e1f0c07f9e9cb76
sankar kalla <gowrisankarkalla4@gmail.com>
2025-11-13 14:35:40 +0530

    add install smallest package

--- scripts/cron-run.ts
+++ scripts/cron-run.ts
@@ -1,5 +1,8 @@
+import smallest from "smallest";
 console.log("started");
 
 const DB_URL = process.env.DB_URL;
 
 console.log(DB_URL, "url of db");
+
+console.log(smallest(1, 2, 3, 10, -1, 2, -10), "is smallest one");
